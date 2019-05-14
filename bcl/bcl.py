bef82305c20df9bcbc09d8578c6c86e2ac767049
Brent Hurst <GBrentHurst@gmail.com>
2019-05-13 20:42:24 -0400

    SetTitle() seems to be working

--- bcl/bcl.py
+++ bcl/bcl.py
@@ -4,22 +4,15 @@ from sys import stdin,stdout,stderr
 
 buf = ''
 tmptitle = 'THETITLESHOULDGOHERE'
+linecount = 0
 
 mainoptions = {}
-# mainoptions['|T'] =
-# mainoptions['|V'] =
-# mainoptions['|v'] =
-# mainoptions['|p'] =
-# mainoptions['{c'] =
-#
-# suboptions = {}
-# suboptions['|t'] =
-# suboptions['{V'] =
-# suboptions['{v'] =
+suboptions = {}
 
 def Setup():
     global buf
-    buf += "<!DOCTYPE html>\n<html>\n\t<title>" + tmptitle + "</title>\n\n\t<meta charset=\"UTF-8\">\n\n"
+    buf += f"<!DOCTYPE html>\n<html>\n\t<title>{tmptitle}</title>\n\n\t<meta charset=\"UTF-8\">\n\n"
+    buf += f"\t<head>\n\t\t<h1>{tmptitle}</h1>\n\t</head>\n\n"
     # put in style tags here
     buf += "\t<body>\n"
 
@@ -27,23 +20,81 @@ def Finish():
     global buf
     buf += "\t</body>\n</html>"
 
+def GetNextLine():
+    global linecount
+    line = stdin.readline()
+    linecount += 1
+    return line
+
+def SetTitle(title):
+    global buf
+    buf = buf.replace(tmptitle,title)
+
+def SetHeadVerse():
+    pass
+
+def SetVerse():
+    pass
+
+def NewPoint():
+    pass
+
+def StartSeeOther():
+    pass
+
+def SetTranslation():
+    pass
+
+def StartHeadVerse():
+    pass
+
+def StartVerse():
+    pass
+
+def FullHeadVerse():
+    pass
+
+def FullVerse():
+    pass
+
+mainoptions['|T'] = SetTitle
+mainoptions['|V'] = SetHeadVerse
+mainoptions['|v'] = SetVerse
+mainoptions['|p'] = NewPoint
+mainoptions['{c'] = StartSeeOther
+
+suboptions['|t'] = SetTranslation
+suboptions['{V'] = StartHeadVerse
+suboptions['{v'] = StartVerse
+suboptions['=V'] = FullHeadVerse
+suboptions['=v'] = FullVerse
+
 
 
 def main():
+    global mainoptions
+    global buf
+
     Setup()
 
     while True:
-        line = stdin.readline()
+        line = GetNextLine()
 
         if not line:
             Finish()
             print(buf)
             break
 
-        command,line = line.strip().split(' ',1)
-        # if command[0] not in ['|', '{', '=']:
+        if ' ' not in line:
+            stderr.write(f'Bad format at line {linecount}:\n{line}')
+            exit(1)
 
+        command,realline = line.strip().split(' ',1)
+        if command not in mainoptions:
+            stderr.write(f'Unexpected command at line {linecount}:\n{line}')
+            exit(2)
 
+        mainoptions[command](realline)
 
 if __name__ == "__main__":
     main()
